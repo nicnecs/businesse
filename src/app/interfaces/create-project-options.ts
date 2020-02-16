64569a305c1bced5033849cb78878d40ea2f96a3
Yannick Synowzik <synowzik@heptacom.de>
2020-02-16 14:20:49 +0100

    add ionic/storage functionality

---
+++ src/app/interfaces/create-project-options.ts
@@ -0,0 +1,12 @@
+export interface CreateProjectOptions {
+    title: string;
+    company: string;
+    description: string;
+    project_start: undefined;
+    project_end: undefined;
+    registration_period: undefined;
+    address: string;
+    address_comment: string;
+    payment: number;
+    number_participants: number;
+}
--- src/app/project-create/project-create.page.ts
+++ src/app/project-create/project-create.page.ts
@@ -1,17 +1,44 @@
 import { Component, OnInit } from '@angular/core';
-import { NgForm } from '@angular/forms';
-import { Router } from '@angular/router';
+
+import { CreateProjectOptions } from "../interfaces/create-project-options";
+import {Router} from "@angular/router";
+import {ProjectData} from "../providers/project-data";
+import {NgForm} from "@angular/forms";
 
 @Component({
   selector: 'app-project-create',
   templateUrl: './project-create.page.html',
   styleUrls: ['./project-create.page.scss'],
 })
-export class ProjectCreatePage implements OnInit {
+export class ProjectCreatePage {
+  createProject: CreateProjectOptions = {
+    title: '',
+    company: '',
+    description: '',
+    project_start: null,
+    project_end: null,
+    registration_period: null,
+    address: '',
+    address_comment: '',
+    payment: null,
+    number_participants: null
+  };
+  submitted = false;
 
-  constructor() { }
+  constructor(
+      public router: Router,
+      public projectData: ProjectData,
+  ) { }
 
-  ngOnInit() {
+  onCreateProject(form: NgForm) {
+    this.submitted = true;
+
+    if (form.valid) {
+      this.projectData.createProject(this.createProject.title);
+      /**
+       * todo navigateByURL
+       */
+    }
   }
 
 }
