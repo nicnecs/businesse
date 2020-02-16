b513ddd477070a38c8e59dab45cdf438b6f9cba7
nicnecs <n-tabeling@web.de>
2020-02-16 21:48:03 +0100

    no commit message

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -9,4 +9,38 @@ export class HomePage {
 
   constructor() {}
 
+  id = 0;
+
+  json = 
+  
+  [
+    {
+      "id" : 5,
+      "name" : "Ein cooles Projekt",
+      "category" : "Programming",
+      "company" : "TeaSausage24"
+    },
+    {
+      "id" : 29,
+      "name" : "Super cooles Projekt",
+      "category" : "Handwerk",
+      "company" : "Microsoft"
+    },
+    {
+      "id" : 19000,
+      "name" : "Super Duper cooles Projekt",
+      "category" : "fff",
+      "company" : "Apppple",
+      "img" : "img",
+      "frist" : "17.02.2020",
+      "project-start" : "20.02.2020"
+    }
+  ]
+
+  toProject(project_id){
+
+    this.id = project_id;
+
+  }
+
 }
