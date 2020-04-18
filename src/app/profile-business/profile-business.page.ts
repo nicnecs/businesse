5cae3323a80da93d4bb6f0aabc916b1e94eae254
Yannick Synowzik <synowzik@heptacom.de>
2020-04-18 13:54:33 +0200

    basic business profile page

--- src/app/profile-business/profile-business.page.ts
+++ src/app/profile-business/profile-business.page.ts
@@ -9,6 +9,21 @@ export class ProfileBusinessPage implements OnInit {
 
   constructor() { }
 
+  business =
+      {
+        "name": "Muster GmbH",
+        "description": "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor " +
+            "invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo" +
+            " dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. " +
+            "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et " +
+            "dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet " +
+            "clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.",
+        "emailAddress": "muster@web.de",
+        "tel": "01234567890",
+        "websiteURL": "https:www.google.com",
+      }
+  ;
+
   ngOnInit() {
   }
 
