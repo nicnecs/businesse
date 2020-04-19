36322ea5e4c439095c3767ec7ba84eec6e4c1e26
Nick Tabeling <ntabeling@merentis.com>
2020-04-19 22:18:18 +0200

    Login als Dialog

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-root',
@@ -13,7 +14,8 @@ export class AppComponent {
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
+    public alertctrl : AlertController
   ) {
     this.initializeApp();
   }
@@ -24,4 +26,51 @@ export class AppComponent {
       this.splashScreen.hide();
     });
   }
+
+  async loginAlert(){
+      await this.alertctrl.create({
+      header: 'Login',
+      animated: true,
+      backdropDismiss: true,
+      mode: 'md',
+      inputs: [
+        {
+          name: 'email',
+          type: 'email',
+          placeholder: 'Email'
+        },
+        {
+          name: 'password',
+          type: 'password',
+          placeholder: 'Passwort'
+        }
+      ],
+      buttons: [
+        {
+          text: 'Schließen',
+          role: 'cancel',
+          cssClass: 'Primary',
+          handler: () => {
+            console.log('Confirm Cancel');
+          }
+        }, {
+          text: 'Login',
+          handler: (res) => {
+            console.log(res.email);
+            this.login(res.email, res.password);
+            
+          }
+        }
+      ]
+    }).then(res => res.present());
+
+    
+  }
+
+  logindata = "Email Pass"
+
+  login(email, pass){
+    this.logindata = email + " " + pass;
+  }
+
 }
