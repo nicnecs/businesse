d5710875d9aa0c040b3376e3512eb958c3c46714
Yannick Synowzik <synowzik@heptacom.de>
2020-05-09 18:55:55 +0200

    Datenschutz, Impressum, AGB(zz unbrauchbar)

---
+++ src/app/agb/agb.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-agb',
+  templateUrl: './agb.page.html',
+  styleUrls: ['./agb.page.scss'],
+})
+export class AgbPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
